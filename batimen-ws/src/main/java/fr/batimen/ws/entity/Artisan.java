5d6a0edd148b3854fff5e21db6f6312445881d5d
raiden <raiden0610@gmail.com>
2014-02-21 19:57:59 +0100

    Correction duplication entité

--- batimen-ws/src/main/java/fr/batimen/ws/entity/Artisan.java
+++ batimen-ws/src/main/java/fr/batimen/ws/entity/Artisan.java
@@ -2,7 +2,6 @@ package fr.batimen.ws.entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -136,111 +135,6 @@ public class Artisan extends AbstractUser implements Serializable {
 	 */
 	public void setDomaineActivite(String domaineActivite) {
 		this.domaineActivite = domaineActivite;
-	}
-
-	/**
-	 * @return the nom
-	 */
-	public String getNom() {
-		return nom;
-	}
-
-	/**
-	 * @param nom
-	 *            the nom to set
-	 */
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	/**
-	 * @return the prenom
-	 */
-	public String getPrenom() {
-		return prenom;
-	}
-
-	/**
-	 * @param prenom
-	 *            the prenom to set
-	 */
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	/**
-	 * @return the numeroTel
-	 */
-	public String getNumeroTel() {
-		return numeroTel;
-	}
-
-	/**
-	 * @param numeroTel
-	 *            the numeroTel to set
-	 */
-	public void setNumeroTel(String numeroTel) {
-		this.numeroTel = numeroTel;
-	}
-
-	/**
-	 * @return the login
-	 */
-	public String getLogin() {
-		return login;
-	}
-
-	/**
-	 * @param login
-	 *            the login to set
-	 */
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @param password
-	 *            the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email
-	 *            the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * @return the dateInscription
-	 */
-	public Date getDateInscription() {
-		return dateInscription;
-	}
-
-	/**
-	 * @param dateInscription
-	 *            the dateInscription to set
-	 */
-	public void setDateInscription(Date dateInscription) {
-		this.dateInscription = dateInscription;
 	}
 
 	/**
