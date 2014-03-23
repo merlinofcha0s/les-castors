d21112affe8dbc1ffe52ca49139ff4f808ceb281
raiden <raiden0610@gmail.com>
2014-03-23 18:17:26 +0100

    Ajout du support de dbunit dans arquillian

--- batimen-ws/src/test/java/fr/batimen/ws/facade/GestionClientFacadeTest.java
+++ batimen-ws/src/test/java/fr/batimen/ws/facade/GestionClientFacadeTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertTrue;
 import java.util.Calendar;
 import java.util.Locale;
 
-import org.junit.Before;
+import org.jboss.arquillian.persistence.Cleanup;
+import org.jboss.arquillian.persistence.CleanupStrategy;
+import org.jboss.arquillian.persistence.TestExecutionPhase;
+import org.jboss.arquillian.persistence.UsingDataSet;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,70 +18,34 @@ import fr.batimen.dto.LoginDTO;
 import fr.batimen.dto.enums.Civilite;
 import fr.batimen.ws.AbstractBatimenWsTest;
 import fr.batimen.ws.client.service.ClientService;
-import fr.batimen.ws.entity.Client;
 
 /**
  * 
  * @author Casaucau Cyril
  * 
  */
+@Cleanup(phase = TestExecutionPhase.AFTER, strategy = CleanupStrategy.USED_ROWS_ONLY)
 public class GestionClientFacadeTest extends AbstractBatimenWsTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GestionClientFacadeTest.class);
 
-	private final Client clientToRec = new Client();
-
-	private final Calendar cal = Calendar.getInstance(Locale.FRANCE);
-
-	// Creation d'un user de test dans la BDD
-	@Before
-	public void init() throws Exception {
-
-		utx.begin();
-		entityManager.joinTransaction();
-
-		cal.set(Calendar.DAY_OF_MONTH, 13);
-		cal.set(Calendar.MONTH, Calendar.JANUARY);
-		cal.set(Calendar.YEAR, 2014);
-
-		cal.set(Calendar.HOUR_OF_DAY, 00);
-		cal.set(Calendar.MINUTE, 01);
-		cal.set(Calendar.SECOND, 00);
-
-		clientToRec.setEmail("lol@lol.com");
-		clientToRec.setLogin("pebron");
-		clientToRec.setPassword("lollollol");
-		clientToRec.setCivilite(Civilite.MONSIEUR);
-		clientToRec.setPrenom("Pebron");
-		clientToRec.setNom("De la Pebronne");
-		clientToRec.setNumeroTel("0615125645");
-		clientToRec.setDateInscription(cal.getTime());
-		clientToRec.setIsArtisan(false);
-
-		// On persiste un utilisateur dans la bdd
-		entityManager.persist(clientToRec);
-
-		utx.commit();
-		// clear the persistence context (first-level cache)
-		entityManager.clear();
-
-	}
-
 	@Test
+	@UsingDataSet("datasets/clients.yml")
 	public void testGetClientForLogin() {
 
 		// L'objet que l'on doit recevoir du frontend quand l'utilisateur
 		// tentera de s'authentifier
 		LoginDTO toLogin = new LoginDTO();
-		toLogin.setLogin("pebron");
+		toLogin.setLogin("pebronne");
 		toLogin.setPassword("lollollol");
 
 		// Appel du service qui check le login
 		ClientDTO user = ClientService.login(toLogin);
 
 		// Verification des infos
-		assertTrue(user.getLogin().equals("pebron"));
-		assertTrue("lollollol".equals(user.getPassword()));
+		assertTrue(user.getLogin().equals("pebronne"));
+		assertTrue("$s0$54040$h99gyX0NNTBvETrAdfjtDw==$fo2obQTG56y7an9qYl3aEO+pv3eH6p4hLzK1xt8EuoY=".equals(user
+		        .getPassword()));
 		assertTrue(user.getEmail().equals("lol@lol.com"));
 		assertTrue(user.getNumeroTel().equals("0615125645"));
 		assertTrue(user.getCivilite() == Civilite.MONSIEUR);
@@ -88,11 +55,20 @@ public class GestionClientFacadeTest extends AbstractBatimenWsTest {
 		Calendar calInscription = Calendar.getInstance(Locale.FRANCE);
 		calInscription.setTime(user.getDateInscription());
 
-		assertTrue(calInscription.get(Calendar.DAY_OF_WEEK) == cal.get(Calendar.DAY_OF_WEEK));
+		Calendar calAssert = Calendar.getInstance(Locale.FRANCE);
+		calAssert.set(Calendar.DAY_OF_MONTH, 10);
+		calAssert.set(Calendar.MONTH, Calendar.JANUARY);
+		calAssert.set(Calendar.YEAR, 2014);
+
+		// On check que la date d'inscription est bien le 10 janvier 2014
+		assertTrue(calInscription.get(Calendar.DAY_OF_WEEK) == calAssert.get(Calendar.DAY_OF_WEEK));
+		assertTrue(calInscription.get(Calendar.MONTH) == calAssert.get(Calendar.MONTH));
+		assertTrue(calInscription.get(Calendar.YEAR) == calAssert.get(Calendar.YEAR));
 
 	}
 
 	@Test
+	@UsingDataSet("datasets/clients.yml")
 	public void testgetClientForLoginFail() throws Exception {
 
 		// L'objet que l'on doit recevoir du frontend quand l'utilisateur
