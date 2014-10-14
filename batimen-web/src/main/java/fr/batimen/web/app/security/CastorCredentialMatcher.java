1655778776b5c97a0104fb5009fc419d88789c77
raiden <raiden0610@gmail.com>
2014-10-14 14:23:19 +0200

    Debut intégration apache shiro (refs #48)

---
+++ batimen-web/src/main/java/fr/batimen/web/app/security/CastorCredentialMatcher.java
@@ -0,0 +1,39 @@
+package fr.batimen.web.app.security;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.batimen.core.security.HashHelper;
+
+public class CastorCredentialMatcher implements CredentialsMatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CastorCredentialMatcher.class);
+
+    @Override
+    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
+        String usernameForm = usernamePasswordToken.getUsername();
+        String passwordForm = String.valueOf(usernamePasswordToken.getPassword());
+
+        SimpleAuthenticationInfo usernamePasswordDB = (SimpleAuthenticationInfo) info;
+        String usernameDB = String.valueOf(usernamePasswordDB.getPrincipals().getPrimaryPrincipal());
+        String passwordDB = String.valueOf(usernamePasswordDB.getCredentials());
+
+        // Vérification du password avec le hash qui se trouve dans la bdd
+        boolean usernameMatch = usernameForm.equals(usernameDB);
+        boolean passwordMatch = HashHelper.check(passwordForm, passwordDB);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Verification de l'username : " + usernameMatch);
+            LOGGER.debug("Verification du password : " + passwordMatch);
+        }
+
+        return usernameMatch && passwordMatch;
+    }
+}
---
+++ batimen-web/src/main/java/fr/batimen/web/app/security/CastorRealm.java
@@ -0,0 +1,45 @@
+package fr.batimen.web.app.security;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import fr.batimen.dto.ClientDTO;
+import fr.batimen.dto.LoginDTO;
+import fr.batimen.ws.client.service.ClientService;
+
+public class CastorRealm extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        SimpleAuthenticationInfo info = null;
+
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authcToken;
+        String usernameForm = usernamePasswordToken.getUsername();
+
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setLogin(usernameForm);
+
+        ClientDTO clientDTO = ClientService.login(loginDTO);
+
+        // Si l'user dto est vide cela veut dire que l'authentification n'a pas
+        // reussi
+        if (!"".equals(clientDTO.getLogin())) {
+            info = new SimpleAuthenticationInfo(clientDTO.getLogin(), clientDTO.getPassword().toCharArray(), getName());
+        }
+
+        return info;
+    }
+
+}
