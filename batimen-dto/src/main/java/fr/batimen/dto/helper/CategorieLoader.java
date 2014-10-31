2ff5f171767d4102e30bd03817adc0850083347b
raiden <raiden0610@gmail.com>
2014-10-31 20:27:00 +0100

    Mise en place des sous categories pour les nouveaux devis (refs #80)

--- batimen-dto/src/main/java/fr/batimen/dto/helper/CategorieLoader.java
+++ batimen-dto/src/main/java/fr/batimen/dto/helper/CategorieLoader.java
@@ -30,8 +30,24 @@ public class CategorieLoader {
     public static synchronized CategorieMetierDTO getCategorieElectricite() {
         if (electricite == null) {
             electricite = new CategorieMetierDTO("Electricité", (short) 0);
-            SousCategorieMetierDTO sousElectrique = new SousCategorieMetierDTO("Installation électrique");
-            electricite.addSousCategorie(sousElectrique);
+            SousCategorieMetierDTO tableauxElectriques = new SousCategorieMetierDTO("Tableaux électriques");
+            SousCategorieMetierDTO reseauxBranchementElectrique = new SousCategorieMetierDTO(
+                    "Réseaux-branchements électriques");
+            SousCategorieMetierDTO domotiques = new SousCategorieMetierDTO("Domotiques");
+            SousCategorieMetierDTO interphone = new SousCategorieMetierDTO("Interphone");
+            SousCategorieMetierDTO antenneSatellite = new SousCategorieMetierDTO("Antenne et Satellite");
+            SousCategorieMetierDTO alarme = new SousCategorieMetierDTO("Alarme");
+            SousCategorieMetierDTO videoSurveillance = new SousCategorieMetierDTO("Vidéo surveillance");
+            SousCategorieMetierDTO desenfumage = new SousCategorieMetierDTO("Desenfumage");
+
+            electricite.addSousCategorie(tableauxElectriques);
+            electricite.addSousCategorie(reseauxBranchementElectrique);
+            electricite.addSousCategorie(domotiques);
+            electricite.addSousCategorie(interphone);
+            electricite.addSousCategorie(antenneSatellite);
+            electricite.addSousCategorie(alarme);
+            electricite.addSousCategorie(videoSurveillance);
+            electricite.addSousCategorie(desenfumage);
         }
         return electricite;
     }
@@ -39,8 +55,37 @@ public class CategorieLoader {
     public static synchronized CategorieMetierDTO getCategoriePlomberie() {
         if (plomberie == null) {
             plomberie = new CategorieMetierDTO("Plomberie", (short) 1);
-            SousCategorieMetierDTO sousPlomberie = new SousCategorieMetierDTO("Tuyauterie");
+            SousCategorieMetierDTO sousPlomberie = new SousCategorieMetierDTO("Plomberie");
+            SousCategorieMetierDTO petitTravaux = new SousCategorieMetierDTO("Petit travaux");
+            SousCategorieMetierDTO installation = new SousCategorieMetierDTO("Installation");
+            SousCategorieMetierDTO cumulus = new SousCategorieMetierDTO("Cumulus");
+            SousCategorieMetierDTO chauffeEau = new SousCategorieMetierDTO("Chauffe eau / solaire");
+            SousCategorieMetierDTO adoucisseurEau = new SousCategorieMetierDTO("Adoucisseur d'eau");
+            SousCategorieMetierDTO chaudiere = new SousCategorieMetierDTO("Chaudière");
+            SousCategorieMetierDTO salleDeBain = new SousCategorieMetierDTO("Salle de bain");
+            SousCategorieMetierDTO sanitaires = new SousCategorieMetierDTO("Sanitaires");
+            SousCategorieMetierDTO wc = new SousCategorieMetierDTO("W.C");
+            SousCategorieMetierDTO sauna = new SousCategorieMetierDTO("Sauna");
+            SousCategorieMetierDTO hamman = new SousCategorieMetierDTO("Hamman");
+            SousCategorieMetierDTO climatisation = new SousCategorieMetierDTO("Climatisation");
+            SousCategorieMetierDTO chambreFroide = new SousCategorieMetierDTO("Chambre froide");
+            SousCategorieMetierDTO vmc = new SousCategorieMetierDTO("VMC");
+
             plomberie.addSousCategorie(sousPlomberie);
+            plomberie.addSousCategorie(petitTravaux);
+            plomberie.addSousCategorie(installation);
+            plomberie.addSousCategorie(cumulus);
+            plomberie.addSousCategorie(chauffeEau);
+            plomberie.addSousCategorie(adoucisseurEau);
+            plomberie.addSousCategorie(chaudiere);
+            plomberie.addSousCategorie(salleDeBain);
+            plomberie.addSousCategorie(sanitaires);
+            plomberie.addSousCategorie(wc);
+            plomberie.addSousCategorie(sauna);
+            plomberie.addSousCategorie(hamman);
+            plomberie.addSousCategorie(climatisation);
+            plomberie.addSousCategorie(chambreFroide);
+            plomberie.addSousCategorie(vmc);
         }
         return plomberie;
     }
@@ -48,8 +93,30 @@ public class CategorieLoader {
     public static synchronized CategorieMetierDTO getCategorieEspaceVert() {
         if (espaceVert == null) {
             espaceVert = new CategorieMetierDTO("Espace Vert", (short) 2);
-            SousCategorieMetierDTO sousEspaceVert = new SousCategorieMetierDTO("Jardinage");
-            espaceVert.addSousCategorie(sousEspaceVert);
+            SousCategorieMetierDTO cloture = new SousCategorieMetierDTO("Cloture");
+            SousCategorieMetierDTO elagage = new SousCategorieMetierDTO("Elagage, Debroussaillage");
+            SousCategorieMetierDTO arrosageAutomatique = new SousCategorieMetierDTO("Arrosage automatique");
+            SousCategorieMetierDTO pepinieriste = new SousCategorieMetierDTO("Pépinieriste");
+            SousCategorieMetierDTO entretienJardin = new SousCategorieMetierDTO("Entretien jardin");
+            SousCategorieMetierDTO creationPiscine = new SousCategorieMetierDTO("Création piscine");
+            SousCategorieMetierDTO poseLiner = new SousCategorieMetierDTO("Pose d'un liner");
+            SousCategorieMetierDTO localTechnique = new SousCategorieMetierDTO("Local technique");
+            SousCategorieMetierDTO entretienPiscine = new SousCategorieMetierDTO("Entretien piscine");
+            SousCategorieMetierDTO couverturePiscine = new SousCategorieMetierDTO("Couverture piscine");
+            SousCategorieMetierDTO poseMargelle = new SousCategorieMetierDTO("Pose d'une margelle");
+
+            espaceVert.addSousCategorie(cloture);
+            espaceVert.addSousCategorie(elagage);
+            espaceVert.addSousCategorie(arrosageAutomatique);
+            espaceVert.addSousCategorie(pepinieriste);
+            espaceVert.addSousCategorie(entretienJardin);
+            espaceVert.addSousCategorie(entretienJardin);
+            espaceVert.addSousCategorie(creationPiscine);
+            espaceVert.addSousCategorie(poseLiner);
+            espaceVert.addSousCategorie(localTechnique);
+            espaceVert.addSousCategorie(entretienPiscine);
+            espaceVert.addSousCategorie(couverturePiscine);
+            espaceVert.addSousCategorie(poseMargelle);
         }
         return espaceVert;
     }
@@ -66,8 +133,55 @@ public class CategorieLoader {
     public static synchronized CategorieMetierDTO getCategorieGrosOeuvre() {
         if (grosOeuvre == null) {
             grosOeuvre = new CategorieMetierDTO("Gros oeuvre", (short) 4);
-            SousCategorieMetierDTO sousGrosOeuvre = new SousCategorieMetierDTO("Fondation");
-            grosOeuvre.addSousCategorie(sousGrosOeuvre);
+            SousCategorieMetierDTO porteFenetre = new SousCategorieMetierDTO("Porte / Fenètre");
+            SousCategorieMetierDTO pluieDeLumiere = new SousCategorieMetierDTO("Pluie de lumière (Vélux)");
+            SousCategorieMetierDTO portailFerronerie = new SousCategorieMetierDTO("Portail / Ferronerie");
+            SousCategorieMetierDTO serrurier = new SousCategorieMetierDTO("Serrurier");
+            SousCategorieMetierDTO storeVolet = new SousCategorieMetierDTO("Store / Volet");
+            SousCategorieMetierDTO vitrerie = new SousCategorieMetierDTO("Vitrerie");
+            SousCategorieMetierDTO peinture = new SousCategorieMetierDTO("Peinture");
+            SousCategorieMetierDTO revetementDecoratif = new SousCategorieMetierDTO("Revétement décoratif");
+            SousCategorieMetierDTO papierPeintTapisserie = new SousCategorieMetierDTO("Papier peint / Tapisserie");
+            SousCategorieMetierDTO lambris = new SousCategorieMetierDTO("lambris");
+            SousCategorieMetierDTO toiture = new SousCategorieMetierDTO("Toiture");
+            SousCategorieMetierDTO charpente = new SousCategorieMetierDTO("Charpente en bois");
+            SousCategorieMetierDTO etancheite = new SousCategorieMetierDTO("Etanchéité");
+            SousCategorieMetierDTO gouttiere = new SousCategorieMetierDTO("Gouttière");
+            SousCategorieMetierDTO traitementBoisCharpente = new SousCategorieMetierDTO("Traitement bois charpente");
+            SousCategorieMetierDTO combles = new SousCategorieMetierDTO("Combles");
+            SousCategorieMetierDTO constructionBois = new SousCategorieMetierDTO("Construction bois");
+            SousCategorieMetierDTO constructionMaison = new SousCategorieMetierDTO("Construction maison");
+            SousCategorieMetierDTO extensionMaison = new SousCategorieMetierDTO("Extension maison");
+            SousCategorieMetierDTO fondation = new SousCategorieMetierDTO("Fondation");
+            SousCategorieMetierDTO ascenseur = new SousCategorieMetierDTO("Ascenseur");
+            SousCategorieMetierDTO carrelage = new SousCategorieMetierDTO("Carrelage");
+            SousCategorieMetierDTO surelevation = new SousCategorieMetierDTO("Surélévation");
+            SousCategorieMetierDTO constructionAnnexes = new SousCategorieMetierDTO("Construction annexes");
+
+            grosOeuvre.addSousCategorie(porteFenetre);
+            grosOeuvre.addSousCategorie(pluieDeLumiere);
+            grosOeuvre.addSousCategorie(portailFerronerie);
+            grosOeuvre.addSousCategorie(serrurier);
+            grosOeuvre.addSousCategorie(storeVolet);
+            grosOeuvre.addSousCategorie(vitrerie);
+            grosOeuvre.addSousCategorie(peinture);
+            grosOeuvre.addSousCategorie(revetementDecoratif);
+            grosOeuvre.addSousCategorie(papierPeintTapisserie);
+            grosOeuvre.addSousCategorie(lambris);
+            grosOeuvre.addSousCategorie(toiture);
+            grosOeuvre.addSousCategorie(charpente);
+            grosOeuvre.addSousCategorie(etancheite);
+            grosOeuvre.addSousCategorie(gouttiere);
+            grosOeuvre.addSousCategorie(traitementBoisCharpente);
+            grosOeuvre.addSousCategorie(combles);
+            grosOeuvre.addSousCategorie(constructionBois);
+            grosOeuvre.addSousCategorie(constructionMaison);
+            grosOeuvre.addSousCategorie(extensionMaison);
+            grosOeuvre.addSousCategorie(fondation);
+            grosOeuvre.addSousCategorie(ascenseur);
+            grosOeuvre.addSousCategorie(carrelage);
+            grosOeuvre.addSousCategorie(surelevation);
+            grosOeuvre.addSousCategorie(constructionAnnexes);
         }
         return grosOeuvre;
     }
