a532145b8dccae705bf0330e8635ed8526ff0676
raiden <raiden0610@gmail.com>
2015-01-18 23:32:41 +0100

    Page Annonce @ 25% (refs #97)

--- batimen-web/src/main/java/fr/batimen/web/client/extend/connected/Annonce.java
+++ batimen-web/src/main/java/fr/batimen/web/client/extend/connected/Annonce.java
@@ -1,8 +1,13 @@
 package fr.batimen.web.client.extend.connected;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.AuthorizationException;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
 
+import fr.batimen.dto.enums.TypeCompte;
 import fr.batimen.web.client.component.Commentaire;
 import fr.batimen.web.client.component.ContactezNous;
 import fr.batimen.web.client.component.Profil;
@@ -12,8 +17,15 @@ public class Annonce extends MasterPage {
 
     private static final long serialVersionUID = -3604005728078066454L;
 
+    private Link<Void> modifierAnnonce;
+    private AjaxLink<Void> supprimerAnnonce;
+    private AjaxLink<Void> inscrireAnnonce;
+    private Link<Void> envoyerDevis;
+
     private Annonce() {
         super("", "", "Annonce particulier", true, "img/bg_title1.jpg");
+        initComposants();
+        initAction();
     }
 
     public Annonce(PageParameters params) {
@@ -26,8 +38,6 @@ public class Annonce extends MasterPage {
         // s'est inscrit a l'annonce (l'a payé)
         // TODO : Acces en mode degradé : Quelles données sont affichés ? est ce
         // qu'il va juste avoir accés a la liste d'annonce ?
-
-        initComposants();
     }
 
     private void initComposants() {
@@ -41,4 +51,26 @@ public class Annonce extends MasterPage {
         this.add(commentaire);
     }
 
+    private void initAction() {
+        modifierAnnonce = new Link<Void>("modifierAnnonce") {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onClick() {
+                // TODO Pluger la modification de l'annonce une fois page faite
+            }
+
+            @Override
+            public boolean isVisible() {
+                try {
+                    SecurityUtils.getSubject().checkRole(TypeCompte.CLIENT.getRole());
+                    return true;
+                } catch (AuthorizationException ae) {
+                    return false;
+                }
+            }
+        };
+        add(modifierAnnonce);
+    }
 }
