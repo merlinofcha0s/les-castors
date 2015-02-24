f012ec54c760d4f4725498a36a71a2242e28deee
raiden <raiden0610@gmail.com>
2015-02-24 13:58:58 +0100

    Page Annonce @ 83% (refs #97) : Mise en place de quartz OK !!

---
+++ batimen-ws/src/main/java/fr/batimen/ws/quartz/jobs/AnnonceGarbageCollector.java
@@ -0,0 +1,15 @@
+package fr.batimen.ws.quartz.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class AnnonceGarbageCollector implements Job {
+
+    @Override
+    public void execute(JobExecutionContext arg0) throws JobExecutionException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
