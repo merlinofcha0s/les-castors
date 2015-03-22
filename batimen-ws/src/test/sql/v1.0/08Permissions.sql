79c3dbadaba8e64a21cd15d6edfc66d7f555f5dd
raiden <raiden0610@gmail.com>
2015-03-22 23:49:28 +0100

    Mise en place de jeu de test pour intégration
    Upgrade de la version du war plugin de maven

--- batimen-ws/src/test/sql/v1.0/08Permissions.sql
+++ batimen-ws/src/test/sql/v1.0/08Permissions.sql
@@ -1,11 +1,11 @@
 insert into Permission (typecompte, client_fk) 
-values (4, 100000);
-
-insert into Permission (typecompte, client_fk) 
 values (4, 100001);
 
 insert into Permission (typecompte, client_fk) 
 values (4, 100002);
+
+insert into Permission (typecompte, client_fk) 
+values (0, 100003);
 
 insert into Permission (typecompte, artisan_fk) 
 values (3, 100006);
