63ffd4b056cc954e915cb5960908d16f3b6bf6e5
raiden <raiden0610@gmail.com>
2015-07-07 14:02:37 +0200

    Améliorations diverses [refs #143)

--- batimen-web/src/test/java/fr/batimen/web/selenium/TestContactPage.java
+++ batimen-web/src/test/java/fr/batimen/web/selenium/TestContactPage.java
@@ -1,18 +1,23 @@
 package fr.batimen.web.selenium;
 
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.operation.Operation;
 import fr.batimen.web.selenium.common.AbstractITTest;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static com.ninja_squad.dbsetup.Operations.sequenceOf;
 import static org.junit.Assert.assertTrue;
 
 public class TestContactPage extends AbstractITTest {
 
     @Override
     public void prepareDB() throws Exception {
-        // pas d'init BD
+        Operation operation = sequenceOf(DELETE_ALL, INSERT_USER_DATA, INSERT_USER_PERMISSION);
+        DbSetup dbSetup = new DbSetup(getDriverManagerDestination(), operation);
+        dbSetup.launch();
     }
 
     @Test
@@ -29,7 +34,24 @@ public class TestContactPage extends AbstractITTest {
         driver.findElement(By.id("submit2")).click();
         Boolean checkCondition = (new WebDriverWait(driver, AbstractITTest.TEMPS_ATTENTE_AJAX))
                 .until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("span.box_type4"),
-                        "Votre message a été transmis correctement. Vous obtiendrez une réponse sur votre email de contact indiqué."));
+                        "Votre message a été transmis correctement. Nous vous répondrons dans les plus brefs délais."));
+        assertTrue(checkCondition);
+    }
+
+    @Test
+    public void contactSuccessWhenLoggedIn() throws Exception {
+        driver.get(appUrl);
+        connexionApplication("raiden", BON_MOT_DE_PASSE, true);
+
+        driver.get(appUrl + "/contact");
+        driver.findElement(By.name("subject")).clear();
+        driver.findElement(By.name("subject")).sendKeys("Object");
+        driver.findElement(By.name("message")).clear();
+        driver.findElement(By.name("message")).sendKeys("Messajation");
+        driver.findElement(By.id("submit2")).click();
+        Boolean checkCondition = (new WebDriverWait(driver, AbstractITTest.TEMPS_ATTENTE_AJAX))
+                .until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("span.box_type4"),
+                        "Votre message a été transmis correctement. Nous vous répondrons dans les plus brefs délais."));
         assertTrue(checkCondition);
     }
 
@@ -45,7 +67,7 @@ public class TestContactPage extends AbstractITTest {
         driver.findElement(By.id("submit2")).click();
         Boolean checkCondition = (new WebDriverWait(driver, AbstractITTest.TEMPS_ATTENTE_AJAX))
                 .until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("span.box_type6"),
-                        "Le champ 'message' est obligatoire."));
+                        "Veuillez entrer votre message !"));
         assertTrue(checkCondition);
     }
 }
