c7e04486a7b7ddae9474590a5db4624c23c4aaa5
raiden <raiden0610@gmail.com>
2015-10-12 18:46:23 +0200

    Correction faute d'ortographes accueil Changement label inscription artisan

--- batimen-web/src/main/java/fr/batimen/web/client/extend/nouveau/artisan/Etape4Confirmation.java
+++ batimen-web/src/main/java/fr/batimen/web/client/extend/nouveau/artisan/Etape4Confirmation.java
@@ -1,5 +1,6 @@
 package fr.batimen.web.client.extend.nouveau.artisan;
 
+import fr.batimen.web.client.extend.Accueil;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.Image;
@@ -7,8 +8,6 @@ import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.ContextRelativeResource;
-
-import fr.batimen.web.client.extend.Accueil;
 
 /**
  * Etape 4 : Confirme ou informe si il y a eu des erreurs lors de
@@ -52,14 +51,14 @@ public class Etape4Confirmation extends Panel {
     }
 
     public void succesInscription() {
-        messageConfirmation1.setDefaultModelObject("Votre compte a été créé avec succés");
+        messageConfirmation1.setDefaultModelObject("Votre compte a été créé avec succés.");
         messageConfirmation2
-                .setDefaultModelObject("Un email vous a été envoyé, merci de cliquer sur le lien présent dans ce dernier pour activer votre compte");
+                .setDefaultModelObject("Les équipes de les castors vont valider votre dossier, nous vous tiendrons informé dès que votre compte sera activé.");
     }
 
     public void failureInscription() {
-        messageConfirmation1.setDefaultModelObject("Un problème est survenue lors de la création de votre compte");
-        messageConfirmation2.setDefaultModelObject("Veuillez nous excuser de la gène occasionée");
+        messageConfirmation1.setDefaultModelObject("Un problème est survenue lors de la création de votre compte.");
+        messageConfirmation2.setDefaultModelObject("Veuillez nous excuser de la gène occasionée.");
         imageConfirmation.setImageResource(new ContextRelativeResource("img/error.png"));
         imageConfirmation.add(new AttributeModifier("alt", "error"));
         this.addOrReplace(imageConfirmation);
