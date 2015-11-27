b9c4d2c411fb476eeae02e8a3b0de690aae97925
raiden <raiden0610@gmail.com>
2015-11-27 08:14:23 +0100

    Correction test nos objectifs

--- castor-web/src/test/java/fr/castor/web/selenium/statique/TestNosObjectifs.java
+++ castor-web/src/test/java/fr/castor/web/selenium/statique/TestNosObjectifs.java
@@ -20,7 +20,7 @@ public class TestNosObjectifs extends AbstractITTest {
     @Test
     public void testNominalNosObjectifs(){
         driver.get(appUrl + UrlPage.ACCUEIL_URL);
-        findElement(By.linkText("Nos Objectifs")).click();
+        findElement(By.linkText("Nos objectifs")).click();
         Boolean checkCondition = (new WebDriverWait(driver, AbstractITTest.TEMPS_ATTENTE_AJAX))
                 .until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("h1.title"),
                         "NOS OBJECTIFS"));
